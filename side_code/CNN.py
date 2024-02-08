5a0e3e8f1917a82115e7714861f046e710b74204
Gaia Marsich <g.marsich@gmail.com>
2024-02-08 23:35:43 +0100

    Arguing with AlexNet

---
+++ side_code/CNN.py
@@ -0,0 +1,236 @@
+import torch
+from torch import nn
+
+import os
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import random_split, DataLoader
+import torch.optim as optim
+import torch.nn.init as init
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+from PIL import ImageOps
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+
+class CNN1(nn.Module):
+
+    # A model will have an __init__() function, where it instantiates its layers
+
+    def __init__(self):
+        super(CNN1, self).__init__() # the constructor of the parent class (nn.Module) is called to initialize the model properly.
+
+        # Convolutional layer 1: in_channels=1 because we have a greyscale
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1) # from [1] we get the formula: output = ((input - kernel_size + 2*padding)/stride) + 1 => 62*62
+        # ReLU activation after conv1
+        self.relu1 = nn.ReLU() # output: 62*62
+        # Max pooling layer 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 31*31 (from 62/2)
+
+        # Convolutional layer 2
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1) # from [1] we know that output: 29*29
+        # ReLU activation after conv2
+        self.relu2 = nn.ReLU() # output: 29*29
+        # Max pooling layer 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 14*14 (from the test in dim_images.ipynb)
+
+        # Convolutional layer 3
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1) # from [1] we know that output: 12*12
+        # ReLU activation after conv3  
+        self.relu3 = nn.ReLU() # output: 12*12
+
+        # Fully connected layer. 32: number of channels; 12, 12: height and width of the feature map
+        self.fc = nn.Linear(32 * 12 * 12, 15)
+        # Classification layer
+        #self.output = nn.CrossEntropyLoss() #TODO: ma è giusto da mettere? Al momento è tolto
+
+        self.initialize_weights()
+
+
+    def initialize_weights(self):       #TODO: the professor mentioned to avoid normalization, what should I do?
+        for module in self.modules(): # self.modules() comes from nn.Module; to recursively iterate over all the modules
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                init.normal_(module.weight, mean=0, std=0.01) # initial weights drawn from a Gaussian distribution having a mean of 0 and a standard deviation of 0.01
+                init.constant_(module.bias, 0) # set the bias to 0
+
+
+    # A model will have a forward() function
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = self.relu3(x)
+
+        x = x.view(-1, 32 * 12 * 12)  # flatten the tensor before passing to fully connected layers (the size -1 is inferred from other dimensions)
+        
+        x = self.fc(x)
+        #x = self.output(x) #TODO: in caso, da eliminare
+
+        return x
+
+
+
+
+# CNN with batch normalization, different size of convolutional filters
+class CNN2(nn.Module):
+
+    # A model will have an __init__() function, where it instantiates its layers
+
+    def __init__(self):
+        super(CNN2, self).__init__() # the constructor of the parent class (nn.Module) is called to initialize the model properly.
+
+        # Convolutional layer 1: in_channels=1 because we have a greyscale
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1) # from [1] we get the formula: output = ((input - kernel_size + 2*padding)/stride) + 1 => 62*62
+        # batch normalization
+        self.bn1 = nn.BatchNorm2d(8) # the parameter should be equal to the out_channels of the convolution
+        # ReLU activation after conv1
+        self.relu1 = nn.ReLU() # output: 62*62
+        # Max pooling layer 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 31*31 (from 62/2)
+
+        # Convolutional layer 2
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, stride=1) # from [1] we know that output: 29*29
+        # batch normalization
+        self.bn2 = nn.BatchNorm2d(16)
+        # ReLU activation after conv2
+        self.relu2 = nn.ReLU() # output: 29*29
+        # Max pooling layer 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 14*14 (from the test in dim_images.ipynb)
+
+        # Convolutional layer 3
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=7, stride=1) # from [1] we know that output: 12*12
+        # batch normalization
+        self.bn3 = nn.BatchNorm2d(32)
+        # ReLU activation after conv3  
+        self.relu3 = nn.ReLU() # output: 12*12
+
+        # Fully connected layer. 32: number of channels; 12, 12: height and width of the feature map
+        self.fc = nn.Linear(32 * 7 * 7, 15)
+        # Classification layer
+        #self.output = nn.CrossEntropyLoss() #TODO: ma è giusto da mettere? Al momento è tolto
+
+        self.initialize_weights()
+
+
+    def initialize_weights(self):       #TODO: the professor mentioned to avoid normalization, what should I do?
+        for module in self.modules(): # self.modules() comes from nn.Module; to recursively iterate over all the modules
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                init.normal_(module.weight, mean=0, std=0.01) # initial weights drawn from a Gaussian distribution having a mean of 0 and a standard deviation of 0.01
+                init.constant_(module.bias, 0) # set the bias to 0
+
+
+    # A model will have a forward() function
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+
+        x = x.view(-1, 32 * 7 * 7)  # flatten the tensor before passing to fully connected layers (the size -1 is inferred from other dimensions)
+        
+        x = self.fc(x)
+        #x = self.output(x) #TODO: in caso, da eliminare
+
+        return x
+
+
+
+
+
+# CNN with batch normalization, different size of convolutional filters, dropout
+class CNN3(nn.Module):
+
+    # A model will have an __init__() function, where it instantiates its layers
+
+    def __init__(self):
+        super(CNN3, self).__init__() # the constructor of the parent class (nn.Module) is called to initialize the model properly.
+
+        # Convolutional layer 1: in_channels=1 because we have a greyscale
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1) # from [1] we get the formula: output = ((input - kernel_size + 2*padding)/stride) + 1 => 62*62
+        # batch normalization
+        self.bn1 = nn.BatchNorm2d(8) # the parameter should be equal to the out_channels of the convolution
+        # ReLU activation after conv1
+        self.relu1 = nn.ReLU() # output: 62*62
+        # Max pooling layer 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 31*31 (from 62/2)
+
+        # Convolutional layer 2
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, stride=1) # from [1] we know that output: 29*29
+        # batch normalization
+        self.bn2 = nn.BatchNorm2d(16)
+        # ReLU activation after conv2
+        self.relu2 = nn.ReLU() # output: 29*29
+        # Max pooling layer 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 14*14 (from the test in dim_images.ipynb)
+
+        # Convolutional layer 3
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=7, stride=1) # from [1] we know that output: 12*12
+        # batch normalization
+        self.bn3 = nn.BatchNorm2d(32)
+        # ReLU activation after conv3  
+        self.relu3 = nn.ReLU() # output: 12*12
+        # Dropout
+        self.dp = nn.Dropout(.1)
+        # Fully connected layer. 32: number of channels; 12, 12: height and width of the feature map
+        self.fc = nn.Linear(32 * 7 * 7, 15)
+        # Classification layer
+        #self.output = nn.CrossEntropyLoss() #TODO: ma è giusto da mettere? Al momento è tolto
+
+        self.initialize_weights()
+
+
+    def initialize_weights(self):       #TODO: the professor mentioned to avoid normalization, what should I do?
+        for module in self.modules(): # self.modules() comes from nn.Module; to recursively iterate over all the modules
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                init.normal_(module.weight, mean=0, std=0.01) # initial weights drawn from a Gaussian distribution having a mean of 0 and a standard deviation of 0.01
+                init.constant_(module.bias, 0) # set the bias to 0
+
+
+    # A model will have a forward() function
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.dp(x)
+
+        x = x.view(-1, 32 * 7 * 7)  # flatten the tensor before passing to fully connected layers (the size -1 is inferred from other dimensions)
+        
+        x = self.fc(x)
+        #x = self.output(x) #TODO: in caso, da eliminare
+
+        return x
+
---
+++ side_code/train_test.py
@@ -0,0 +1,196 @@
+import torch
+from torch import nn
+
+import os
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import random_split, DataLoader
+import torch.optim as optim
+import torch.nn.init as init
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+from PIL import ImageOps
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+
+
+# To transform the images in the proper format
+def transform(image):
+    resize = transforms.Compose([transforms.Resize([64,64]), # resize the images as 64x64
+                                 transforms.ToTensor(), # to get a PyTorch tensor. The pixels' values will be scaled from the range [0, 255] to the range [0.0, 1.0]
+                                 transforms.Grayscale()]) # the image will just have one channel
+    resized_image = resize(image)
+    resized_image = resized_image * 255.0 # revert the normalization, i.e., go back to range [0, 255]
+    return resized_image
+
+
+
+
+# To transform the images in the proper format
+def transform_ALEXNET(image):
+    resize = transforms.Compose([transforms.Resize([64,64]), # resize the images as 64x64
+                                 transforms.ToTensor()]) # to get a PyTorch tensor. The pixels' values will be scaled from the range [0, 255] to the range [0.0, 1.0]
+    resized_image = resize(image)
+    resized_image = resized_image * 255.0 # revert the normalization, i.e., go back to range [0, 255]
+    return resized_image
+
+
+
+
+# To horizontally flip the images
+class HorizontallyFlippedDataset(Dataset):
+    def __init__(self, original_dataset):
+        self.original_dataset = original_dataset
+        self.horizontal_flip_transform = transforms.RandomHorizontalFlip()
+
+    def __getitem__(self, index):
+        image, label = self.original_dataset[index]
+        flipped_image = self.horizontal_flip_transform(image)
+        return flipped_image, label
+
+    def __len__(self):
+        return len(self.original_dataset)
+
+
+
+
+# To train for just one epoch
+def train_one_epoch(epoch_index, model, loader, optimizer, loss_function): # to train for just one epoch (one epoch: the network sees the whole training set)
+    running_loss = 0.
+
+    for i, data in enumerate(loader):
+
+        inputs, labels = data # get the minibatch
+
+        outputs = model(inputs) # forward pass
+
+        loss = loss_function(outputs, labels) # compute the loss
+        running_loss += loss.item() # sum up the loss for the minibatches processed so far
+
+        optimizer.zero_grad() # notice that by default, the gradients are accumulated, hence we need to set them to zero
+        loss.backward() # backward pass
+        optimizer.step() # update the weights
+
+    return running_loss/(i+1) # average loss per minibatch
+
+
+
+
+# To perform the training
+def train_model(model, EPOCHS, train_loader, val_loader, optimizer, loss_function):
+
+    best_validation_loss = np.inf
+
+    loss_train = [] # store the values of the loss for the training set
+    loss_val = [] # store the values of the loss for the validation set
+    accuracies_val = [] # store the values of the accuracy for the validation set
+
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        train_loss = train_one_epoch(epoch, model, train_loader, optimizer, loss_function)
+        loss_train.append(train_loss)
+
+        running_validation_loss = 0.0
+
+        # If using dropout and/or batch normalization, we need the following, to set the model to evaluation mode, disabling dropout and using population
+        # statistics for batch normalization.
+        model.eval()
+
+
+        correct = 0
+        total = 0
+
+        with torch.no_grad():      # disable gradient computation and reduce memory consumption
+            for i, vdata in enumerate(val_loader):
+                vinputs, vlabels = vdata
+                voutputs = model(vinputs)
+                vloss = loss_function(voutputs, vlabels)
+                running_validation_loss += vloss
+                _, predicted = torch.max(voutputs.data, 1)
+                total += vlabels.size(0)
+                correct += (predicted == vlabels).sum().item()
+        
+        accuracy = 100 * correct / total
+        accuracies_val.append(accuracy)
+
+        validation_loss = running_validation_loss / (i + 1) # average validation loss per minibatch
+        loss_val.append(validation_loss)
+        
+        print('LOSS: train: {}, validation: {}; accuracy validation set: {}%\n'.format(train_loss, validation_loss, accuracy))
+
+
+        # Track best performance (based on validation), and save the model
+        if validation_loss < best_validation_loss:
+            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+            best_validation_loss = validation_loss
+            model_path = 'model_{}_{}'.format(timestamp, epoch)
+            torch.save(model.state_dict(), model_path)
+
+    return model_path, loss_train, loss_val, accuracies_val
+
+
+
+
+# To compute the accuracy on the test dataset and build the confusion matrix
+def perform_test(model, model_path, test_loader, dataset_test):
+    correct_test = 0
+    total_test = 0
+
+    y_pred = []
+    y_true = []
+
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    with torch.no_grad():
+
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total_test += labels.size(0)
+            correct_test += (predicted == labels).sum().item()
+
+            y_pred.extend(predicted)
+            y_true.extend(labels) 
+
+
+    accuracy_test = 100 * correct_test / total_test
+    print('Accuracy of the network on the test images: %d %%' % (accuracy_test))
+
+
+    # Build confusion matrix
+    classes = dataset_test.classes
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index = [i for i in classes],
+                        columns = [i for i in classes])
+    plt.figure(figsize = (5,5))
+    sn.heatmap(df_cm, annot=False)
+    #plt.savefig('output.png')
+
+
+
+
+
+class GrayscaleToRGBDataset(Dataset):
+    def __init__(self, original_dataset):
+        self.original_dataset = original_dataset
+        self.transform = transforms.Compose([transforms.Grayscale(num_output_channels=3)])
+
+    def __len__(self):
+        return len(self.original_dataset)
+
+    def __getitem__(self, idx):
+        img, target = self.original_dataset[idx]
+        img = self.transform(img)
+        return img, target
+
